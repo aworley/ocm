8fbce2bfef6af2034e746df3faa0e8a09dce28fd
Aaron Worley <aaron@pikasoftware.com>
2017-12-08 10:03:38 -0500

    Added sample files to create settings needed for SMS integration.

---
+++ cms/app/sql/misc/sample_sparkpost.sql
@@ -0,0 +1,2 @@
+INSERT INTO settings VALUES ('sparkpost_api_key', '');
+INSERT INTO settings VALUES ('sparkpost_from_address', 'no-reply@legalaid.tld');
---
+++ cms/app/sql/misc/sample_twilio.sql
@@ -0,0 +1,4 @@
+INSERT INTO settings VALUES ('twilio_account_sid', '');
+INSERT INTO settings VALUES ('twilio_auth_token', '');
+INSERT INTO settings VALUES ('twilio_number', '+15551234567');
+
