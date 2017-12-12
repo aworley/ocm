832119157f8026ac29a4c2457f6f659e68112f0b
Aaron Worley <aaron@pikasoftware.com>
2017-12-12 15:53:29 -0500

    Fixed SQL error and modified formatting.

--- cms/app/sql/upgrades/pika607_sms.sql
+++ cms/app/sql/upgrades/pika607_sms.sql
@@ -1,3 +1,3 @@
 INSERT INTO menu_act_type VALUES ('S', 'SMS Message', 5);
-ALTER TABLE `cms`.`cases` 
-ADD COLUMN `unread_sms` TINYINT(4) NOT NULL DEFAULT 0 AFTER `vawa_served`;
+ALTER TABLE cases ADD COLUMN unread_sms TINYINT(4) NOT NULL DEFAULT 0 
+	AFTER vawa_served;
