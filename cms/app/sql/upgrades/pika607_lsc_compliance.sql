58966b3bbffce99061acb9d2c484e7707821d7b8
bmarlowe <brad@pikasoftware.com>
2019-06-07 16:51:39 -0400

    Added 1605 field to LSC Compliance tab.
    Added field to LSC Compliance tab: 45 CFR 1605 - Appeals on behalf of Clients

--- cms/app/sql/upgrades/pika607_lsc_compliance.sql
+++ cms/app/sql/upgrades/pika607_lsc_compliance.sql
@@ -1,5 +1,6 @@
 ALTER TABLE cases 
-ADD COLUMN lsc_cfr_1609 TINYINT default NULL,
+ADD COLUMN lsc_cfr_1605 TINYINT default NULL,
+ADD COLUMN lsc_cfr_1609 TINYINT default NULL AFTER lsc_cfr_1605,
 ADD COLUMN lsc_cfr_1612 TINYINT default NULL AFTER lsc_cfr_1609,
 ADD COLUMN lsc_cfr_1620 TINYINT default NULL AFTER lsc_cfr_1612,
 ADD COLUMN lsc_cfr_1633 TINYINT default NULL AFTER lsc_cfr_1620,
